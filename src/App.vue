cfc9c5535827b8a90532d68fe894fed2a9354ebc
xna <xna00@qq.com>
2020-10-18 22:33:20 +0800

    初始化

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+<Test/>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import Test from './components/Test.vue' 
+export default {
+  name: 'App',
+  components: {
+    HelloWorld,
+    Test
+  },
+  setup(props) {
+  }
+}
+</script>
