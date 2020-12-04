c53b2b1cc7fef39f5db6c7a3163560fb8ddd2996
xna <xna00@qq.com>
2020-12-04 22:32:26 +0800

    Demo组件添加动画，修改复制按钮

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="demo">
-    <span class="copy">复制</span>
+    <Button @click="click" theme="text" class="copy">复制</Button></Button>
     <h2>{{ title }}</h2>
     <div class="demo-component">
       <component :is="component"></component>
@@ -9,14 +9,17 @@
       <Button v-if="!codeVisible" @click="codeVisibleToggle">显示代码</Button>
       <Button v-else @click="codeVisibleToggle">隐藏代码</Button>
     </div>
-    <div class="demo-code" v-if="codeVisible">
-      <pre v-html="html"></pre>
-    </div>
+    <transition name="demo-code">
+      <div class="demo-code" v-if="codeVisible">
+        <pre v-html="html"></pre>
+      </div>
+    </transition>
   </div>
 </template>
 <script lang="ts">
 import Prism from "prismjs";
 import "prismjs/themes/prism.css";
+import "../index.scss"
 
 import Button from "../lib/Button.vue";
 import { ref } from "vue";
@@ -42,6 +45,20 @@ export default {
 </script>
 <style lang="scss" scoped>
 $border-color: #d9d9d9;
+.demo-code-enter-active, .demo-code-leave-active {
+  transition: all .2s linear;
+  // padding: 0;
+}
+
+.demo-code-enter-from, .demo-code-leave-to {
+  // opacity: 0;
+  // transform: translateX(-150px);
+  height: 0;
+  // padding: 0;
+}
+.demo-code-enter-to, .demo-code-leave-from {
+  height: 300px;
+}
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
@@ -52,7 +69,7 @@ $border-color: #d9d9d9;
     top: 3px;
     right: 3px;
     &:hover {
-      border: 1px solid red;
+      // border: 1px solid red;
       cursor: pointer;
     }
   }
