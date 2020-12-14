fd685a357147d3ce4592e16812d4143c732a3c1e
xna <xna00@qq.com>
2020-12-14 22:43:00 +0800

    改进 Demo组件的样式

--- src/components/Demo.vue
+++ src/components/Demo.vue
@@ -1,21 +1,28 @@
 <template>
   <div class="demo">
-    <div :class="['copy-succed-text', { 'copy-succed': copySucced }]">
-      复制成功
-    </div>
-    <Button @click="copy" theme="text" class="copy-button">复制代码</Button>
+    <svg
+      class="icon"
+      :class="{ 'code-visible': codeVisible }"
+      aria-hidden="true"
+      @click="codeVisibleToggle"
+    >
+      <use xlink:href="#icon-code"></use>
+    </svg>
     <h2>{{ title }}</h2>
     <div class="demo-component">
       <component :is="component"></component>
     </div>
-    <div class="demo-actions">
-      <Button @click="codeVisibleToggle">{{
-        !codeVisible ? "显示代码" : "隐藏代码"
-      }}</Button>
-    </div>
     <transition name="demo-code">
       <div class="demo-code" v-if="codeVisible">
-        <pre v-html="html"></pre>
+        <div :class="['copy-succed-text', { 'copy-succed': copySucced }]">
+          复制成功
+        </div>
+        <svg class="icon copy-button" aria-hidden="true" @click="copy">
+          <use xlink:href="#icon-copy2"></use>
+        </svg>
+        <div class="code">
+          <pre v-html="html"></pre>
+        </div>
       </div>
     </transition>
   </div>
@@ -74,15 +81,11 @@ $theme-color: #76a477;
 .demo-code-enter-active,
 .demo-code-leave-active {
   transition: all 0.2s linear;
-  // padding: 0;
 }
 
 .demo-code-enter-from,
 .demo-code-leave-to {
-  // opacity: 0;
-  // transform: translateX(-150px);
   height: 0;
-  // padding: 0;
 }
 .demo-code-enter-to,
 .demo-code-leave-from {
@@ -93,26 +96,17 @@ $theme-color: #76a477;
   margin: 16px 0 32px;
   border-radius: 3px;
   position: relative;
-  > .copy-succed-text {
-    color: $theme-color;
+  > svg {
     position: absolute;
-    right: 90px;
-    top: 30px;
-    font-size: 13.333px;
-    opacity: 0;
-    cursor: auto;
-    &.copy-succed {
-      top: 7px;
-      opacity: 1;
-      transition: all 250ms;
+    top: 8px;
+    right: 6px;
+    width: 25px;
+    height: 25px;
+    &.code-visible {
+      color: $theme-color;
     }
   }
-  > .copy-button {
-    position: absolute;
-    top: 3px;
-    right: 3px;
-    cursor: pointer;
-  }
+
   > h2 {
     font-size: 20px;
     padding: 8px 16px;
@@ -130,13 +124,50 @@ $theme-color: #76a477;
     }
   }
   &-code {
-    padding: 8px 16px;
     border-top: 1px dashed $border-color;
     overflow: auto;
-    > pre {
-      line-height: 1.1;
-      font-family: Consolas, "Courier New", Courier, monospace;
-      margin: 0;
+    position: relative;
+    > .copy-succed-text {
+      color: $theme-color;
+      position: absolute;
+      right: 40px;
+      top: 30px;
+      font-size: 13.333px;
+      opacity: 0;
+      cursor: auto;
+      &.copy-succed {
+        top: 7px;
+        opacity: 1;
+        transition: all 250ms;
+      }
+    }
+    > .copy-button {
+      position: absolute;
+      top: 4px;
+      right: 6px;
+      cursor: pointer;
+      width: 20px;
+      height: 20px;
+
+      @media (any-hover: hover) {
+        &:hover {
+          color: $theme-color;
+        }
+      }
+      &:active {
+        color: $theme-color;
+      }
+    }
+    > .code {
+      width: 100%;
+      height: 100%;
+      overflow: auto;
+      padding: 8px 16px;
+      > pre {
+        line-height: 1.1;
+        font-family: Consolas, "Courier New", Courier, monospace;
+        margin: 0;
+      }
     }
   }
 }
