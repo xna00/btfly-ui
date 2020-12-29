5fdf569f0788a77ca635396cd29824bb84440f8e
xna <xna00@qq.com>
2020-12-29 16:52:23 +0800

    添加 CircleSeekBarDemo1

---
+++ src/demos/seekbar/CircleSeekBar1.vue
@@ -0,0 +1,30 @@
+<demo>
+圆形进度条
+</demo>
+<template>
+  <CircleSeekBar
+    :barRadius="50"
+    :indicatorRadius="5"
+    backgroundColor="green"
+    color="orange"
+    indicatiorColor="grey"
+    :width="2"
+    v-model:process="process"
+  />
+  <div>process:{{ process }}</div>
+</template>
+<script lang="ts">
+import CircleSeekBar from "../../lib/CircleSeekBar.vue";
+export default {
+  components: {
+    CircleSeekBar,
+  },
+  data() {
+    return {
+      process: 0.2,
+    };
+  },
+};
+</script>
+<style lang="scss">
+</style>
