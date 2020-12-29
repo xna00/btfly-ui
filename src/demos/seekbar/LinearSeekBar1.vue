ff11648703cf4ebfeeba10ad2bbc422b0f3c9be1
xna <xna00@qq.com>
2020-12-29 16:53:32 +0800

    添加 LinearSeekBarDemo1

---
+++ src/demos/seekbar/LinearSeekBar1.vue
@@ -0,0 +1,26 @@
+<demo>
+直线进度条
+</demo>
+<template>
+  <LinearSeekBar
+    backgroundColor="black"
+    color="red"
+    :barHeight="4"
+    :barWidth="600"
+    :indicatorRadius="5"
+    indicatiorColor="green"
+  />
+</template>
+<script lang="ts">
+import LinearSeekBar from "../../lib/LinearSeekBar.vue";
+export default {
+  components: {
+    LinearSeekBar,
+  },
+  data() {
+    return {
+      process: 0.2,
+    };
+  },
+};
+</script>
