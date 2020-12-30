59e9e65e996f01e5db1d45c60182ee83f7b8fca9
xna <xna00@qq.com>
2020-12-30 22:17:12 +0800

    添加 ToastDemo

---
+++ src/components/ToastDemo.vue
@@ -0,0 +1,17 @@
+<template>
+   <Demo :component="Toast1"/> 
+   <Demo :component="Toast2"/>
+</template>
+<script lang="ts">
+import Toast1 from '/@demos/toasts/Toast1.vue'
+import Toast2 from '/@demos/toasts/Toast2.vue'
+import Demo from './Demo.vue'
+export default {
+    components:{
+        Demo
+    },
+    setup(){
+        return {Toast1, Toast2}
+    }
+}
+</script>
--- src/router.ts
+++ src/router.ts
@@ -3,6 +3,7 @@ import Doc from './views/Doc.vue'
 import SwitchDemo from './components/SwitchDemo.vue'
 import ButtonDemo from './components/ButtonDemo.vue'
 import DialogDemo from './components/DialogDemo.vue'
+import ToastDemo from './components/ToastDemo.vue'
 import TabsDemo from './components/TabsDemo.vue'
 import Markdown from './components/Markdown.vue'
 import CheckboxDemo from './components/CheckboxDemo.vue'
@@ -37,6 +38,7 @@ export const router = createRouter({
                 { path: 'switch', component: SwitchDemo },
                 { path: 'button', component: ButtonDemo },
                 { path: 'dialog', component: DialogDemo },
+                { path: 'toast', component: ToastDemo },
                 { path: 'tabs', component: TabsDemo },
                 { path: 'checkbox', component: CheckboxDemo },
                 { path: 'seekbar', component: SeekBarDemo }
