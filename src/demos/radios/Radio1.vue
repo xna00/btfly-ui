2ba2cb739df786853e9fc78be380c9d5b0ab6227
xna <xna00@qq.com>
2020-12-31 14:58:00 +0800

    添加 Radio1

---
+++ src/demos/radios/Radio1.vue
@@ -0,0 +1,25 @@
+<demo>
+Radio
+</demo>
+<template>
+  <Radio v-model="picked" value="r1">R1</Radio>
+  <br/>
+  <Radio v-model="picked" value="r2">R2</Radio>
+  <br/>
+  <Radio v-model="picked" value="r3">R3</Radio>
+
+  <div>picked:{{ picked }}</div>
+</template>
+<script lang="ts">
+import Radio from "/@lib/Radio.vue";
+export default {
+  components: {
+    Radio,
+  },
+  data() {
+    return {
+      picked: "r1",
+    };
+  },
+};
+</script>
