62ef93508855c836d7cd71866544ce365a46ca2c
xna <xna00@qq.com>
2020-12-31 13:13:22 +0800

    添加 Checkbox2

--- src/components/CheckboxDemo.vue
+++ src/components/CheckboxDemo.vue
@@ -1,16 +1,18 @@
 <template>
   <div>
     <Demo :component="Checkbox1" />
+    <Demo :component="Checkbox2" />
   </div>
 </template>
 
 <script lang="ts">
 import Demo from "./Demo.vue";
 import Checkbox1 from "../demos/checkbox/Checkbox1.vue";
+import Checkbox2 from '../demos/checkbox/Checkbox2.vue'
 export default {
   components: { Demo },
   setup() {
-    return { Checkbox1 };
+    return { Checkbox1 , Checkbox2};
   },
 };
 </script>
---
+++ src/demos/checkbox/Checkbox2.vue
@@ -0,0 +1,24 @@
+<demo>
+多个Checkbox
+</demo>
+<template>
+  <Checkbox value="1" v-model="values">1</Checkbox>
+  <br />
+  <Checkbox value="2" v-model="values">2</Checkbox>
+  <br />
+  <Checkbox value="3" v-model="values">3</Checkbox>
+  <div>values: {{ values }}</div>
+</template>
+<script lang="ts">
+import Checkbox from "/@lib/Checkbox.vue";
+export default {
+  components: {
+    Checkbox,
+  },
+  data() {
+    return {
+      values: [],
+    };
+  },
+};
+</script>
